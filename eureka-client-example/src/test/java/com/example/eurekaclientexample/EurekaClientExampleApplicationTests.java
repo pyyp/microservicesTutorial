a7299864a82c039a06411fa12fae5d42af6f8c57
yangming <yan.gming@yahoo.com>
2019-11-03 14:43:05 +0100

    Eureka project

---
+++ eureka-client-example/src/test/java/com/example/eurekaclientexample/EurekaClientExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekaclientexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
